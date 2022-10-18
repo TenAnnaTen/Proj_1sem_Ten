0a8b35f3ca81720ed9098564c111abdf7ea1b194
TenAnnaTen <ann.ten.tkdlove@mail.ru>
2022-10-18 16:28:29 +0300

    Initial commit

---
+++ PZ_3/PZ_3_2.py
@@ -0,0 +1,14 @@
+#Даны два числа. Вывести большее из них.
+while True:
+    try:        #обработчик исключений
+        chislo_1, chislo_2 = float(input('Введите два числа. Первое: ')), float(input('Второе: '))
+        if chislo_1 > chislo_2:
+            print(f'Первое число: {chislo_1}. Второе: {chislo_2}. Большее их них: {chislo_1}')
+            break
+        else:
+            print(f'Первое число: {chislo_1}. Второе: {chislo_2}. Большее их них: {chislo_2}')
+            break
+
+    except:  # исключения
+        print('Неравильно введенные данные, попробуйте ещё раз')
+        continue
