da4841ee8ecc54c0ebb44df61c40cf3d95784bad
TenAnnaTen <ann.ten.tkdlove@mail.ru>
2023-03-11 18:53:44 +0300

    add db

--- db/saper.py
+++ db/saper.py
@@ -1,20 +1,26 @@
 import sqlite3 as sq
 from users import u
 
-with sq.connect('saper.db') as con:
-     cur = con.cursor()
+# with sq.connect('saper.db') as con:
+#      cur = con.cursor()
 
-# cur.execute("DROP TABLE IF EXISTS users")
-cur.execute("""CREATE TABLE IF NOT EXISTS users (
-        user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        sex INTEGER NOT NULL DEFAULT 1,
-        old INTEGER,
-        score INTEGER
-    )""")
+# cur.execute("""CREATE TABLE IF NOT EXISTS users (
+#         user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         name TEXT NOT NULL,
+#         sex INTEGER NOT NULL DEFAULT 1,
+#         old INTEGER,
+#         score INTEGER
+#     )""")
 
 # cur.execute("INSERT INTO users VALUES(?, ?, ?, ?, ?)" , (3, 'Маша', 1, 24, 300))
 # con.commit()
 
-con.executemany("INSERT INTO users VALUES(?, ?, ?, ?, ?)", u)
-con.commit()
+# con.executemany("INSERT INTO users VALUES(?, ?, ?, ?, ?)", u)
+# con.commit()
+
+with sq.connect('saper.db') as con:
+     cur = con.cursor()
+     
+cur.execute("SELECT * FROM users WHERE old > 20 AND score > 2000 AND sex == 1")
+result = cur.fetchall()
+print(result)
